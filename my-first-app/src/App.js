eaa315585c3904fa2d535de4e3478106dfbb11b2
Ivan Nejezchleb <ivan.nejezchleb@gooddata.com>
2019-07-16 11:48:21 +0200

    Tumicovo demo

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,57 +1,20 @@
 import React, { Component } from 'react';
 import '@gooddata/react-components/styles/css/main.css';
 import {
-  Treemap, Visualization
+  Visualization
 } from '@gooddata/react-components';
 import './App.css';
-import { Test } from './components/Test';
 
 class App extends Component {
   render() {
     const projectId = 'jm8bsdakbhujk1a254h25a6mtd6orn9g';
 
-    const measure = {
-      measure: {
-        localIdentifier: 'm1',
-        definition: {
-          measureDefinition: {
-            item: {
-              uri: '/gdc/md/jm8bsdakbhujk1a254h25a6mtd6orn9g/obj/62827'
-            }
-          }
-        }
-      }
-    };
-    const view = {
-      visualizationAttribute: {
-        displayForm: {
-          uri: '/gdc/md/jm8bsdakbhujk1a254h25a6mtd6orn9g/obj/324'
-        },
-        localIdentifier: '02b7736f6bef48b1849798e430d837df'
-      }};
-    const stack = {
-      visualizationAttribute: {
-      displayForm: {
-        uri: '/gdc/md/jm8bsdakbhujk1a254h25a6mtd6orn9g/obj/952'
-      },
-      localIdentifier: 'bc5257e06a9342ec99854bd1a53f3262'
-    }};
-
     return (
       <div className="App">
         <header className="App-header">
           <div style={{width: 600, height: 800}}>
-            <Treemap
-              projectId={projectId}
-              measures={[measure]}
-              viewBy={view}
-              segmentBy={stack}
-            />
-          </div>
-          <div style={{width: 600, height: 800}}>
             <Visualization projectId={projectId} uri={'/gdc/md/jm8bsdakbhujk1a254h25a6mtd6orn9g/obj/75599'} />
           </div>
-          <Test />
         </header>
       </div>
     );
