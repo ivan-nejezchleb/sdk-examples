ee6592e850012ce778fe69c4d42f5447d18c5904
Ivan Nejezchleb <ivan.nejezchleb@gooddata.com>
2019-10-07 08:39:28 +0200

    BUGFIX: MC-31 Reproducer

--- my-first-app/src/setupProxy.js
+++ my-first-app/src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware');
-const backend = 'secure.gooddata.com';
+const backend = 'frauddashboard-dev.na.gooddata.com';
 
 module.exports = function (app) {
     app.use(proxy("/gdc",  {
